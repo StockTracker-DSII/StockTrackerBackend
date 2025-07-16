69691be8f18fbbd65a9999392e3e0838cd038997
juanrv01 <juan.e.rodriguez@correounivalle.edu.co>
2025-07-16 16:51:44 -0500

    Add render.yaml

--- config/config.js
+++ config/config.js
@@ -17,4 +17,14 @@ module.exports = {
     port: 5432,
     dialect: 'postgres'
   },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
