e7a97a7c931c7b992530d6ce01bf87cebbeee7e4
juanrv01 <juan.e.rodriguez@correounivalle.edu.co>
2025-07-16 21:53:08 -0500

    late

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-const { sequelize } = require('./models');
+const { sequelize } = require('../models');
 
 sequelize.sync();
 app.listen(PORT, () => {
